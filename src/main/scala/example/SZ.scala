b1f0ebbc25e2193df386027c4dd9384deef689ce
Crescentweiss <601923796@qq.com>
2024-09-20 11:20:22 -0400

    1st version for interpolation prdiction

--- src/main/scala/example/SZ.scala
+++ src/main/scala/example/SZ.scala
@@ -1,9 +1,262 @@
 package example
 
-object SZ extends Greeting with App {
-  println(greeting)
+import scala.collection.mutable
+import scala.collection.mutable.PriorityQueue
+import scala.util.control.Breaks._
+
+object SZData {
+  // Define a dataset of integers with initial values
+    var dataset: Array[Double] = Array(
+        1.53, 2.34, 3.65,
+        4.17, 5.92, 6.78,
+        7.89, 8.90, 9.01
+    )
+    val eb_2 = 0.2  // 2*eb, eb = 0.1
+    val N = 9  // 2^n + 1, every cycle, input data size <= N
+    var dataprediction: Array[Double] = Array.fill(dataset.length)(0.0)
+    var datadelta: Array[Double] = Array.fill(dataset.length)(0.0)
+    var dataquantization: Array[Int] = Array.fill(dataset.length)(0)
+    var datareconstruction: Array[Double] = Array.fill(dataset.length)(0.0)
+    var flagreconstruction: Array[Boolean] = Array.fill(dataset.length)(false) // true: reconstructed, false: not reconstructed
+    var encodedictionary: mutable.Map[Int, String] = mutable.Map()
+    var datacompression: Int = -1  // -1 means empty
 }
 
-trait Greeting {
-  lazy val greeting: String = "hello"
+object SZprediction extends App {
+    import SZData._
+
+    def LinearInterpolationPredictor(reconstructeddata: Array[Double], startindex: Int, midindex: Int, endindex: Int): Double = {
+        val size = endindex - startindex
+        val left = midindex - startindex
+        val right = endindex - midindex
+        val datalength = reconstructeddata.length
+
+        if (midindex == startindex) {
+            dataprediction(midindex) = 0.0  // start, p1 = 0
+            return dataprediction(midindex)
+        } else if (midindex == endindex) {
+            dataprediction(midindex) = reconstructeddata(0) // n = log2(N), pN = p1
+            return dataprediction(midindex)
+        } else {
+            dataprediction(midindex) = reconstructeddata(startindex) * (right) / size + reconstructeddata(endindex) * (left) / size
+            return dataprediction(midindex)
+        }
+    }
+}
+
+object SZdelta extends App {
+    import SZData._
+    // calculate the difference between this cycle predicted value and the last cycle predicted value
+    def delta(actual: Double, predicted: Double): Double = {
+        return (actual - predicted)
+    }
+}
+
+object SZquantization extends App {
+    import SZData._
+    // quantize the predicted value
+    def quantize(value: Double): Int = {
+        return Math.round(value/SZData.eb_2).toInt 
+    }
+}
+
+object SZreconstruction extends App {
+    import SZData._
+    // restore data by last cycle predicted value + reconstructed quantized delta
+    def reconstructData(predictedData: Double, quantizedDelta: Int): Double = {
+        val reconstructeddata = predictedData + quantizedDelta*eb_2
+        return reconstructeddata
+    }
+
+    def Truereconstruction(index: Int): Unit = {
+        flagreconstruction(index) = true  // reconstructed
+    }
+
+    def Falsereconstruction(): Unit = {
+        for (index <- dataset.indices) {
+            flagreconstruction(index) = false  // new reconstruction step, not reconstructed
+        }
+    }
+}
+
+object SZpipeline extends App {
+    import SZData._
+    import SZprediction._
+    import SZdelta._
+    import SZquantization._
+    import SZreconstruction._
+    
+    // restore data by last cycle predicted value + reconstructed quantized delta
+    def quantizationpipeline(): Array[Int] = {
+        val Ncurrent = dataset.length
+
+        if (Ncurrent == N) {
+            val n = Math.ceil(Math.log(N) / Math.log(2)).toInt + 1
+            for (i <- n to 0 by -1) {
+                for (index <- 0 to N by Math.pow(2, i).toInt) {
+                    val startindex = index
+                    val midindex = if (index + Math.pow(2, i-1).toInt > N - 1) 0 else index + Math.pow(2, i-1).toInt
+                    val endindex = Math.min(index + Math.pow(2, i).toInt, N - 1)
+                    breakable {
+                        if (midindex < 0 || midindex >= N || flagreconstruction(midindex)) {
+                            // to the next cycle
+                            break()
+                        }
+                        dataprediction(midindex) = LinearInterpolationPredictor(datareconstruction, startindex, midindex, endindex)
+                        datadelta(midindex) = delta(dataset(midindex), dataprediction(midindex))
+                        dataquantization(midindex) = quantize(datadelta(midindex))
+                        datareconstruction(midindex) = reconstructData(dataprediction(midindex), dataquantization(midindex))
+                        Truereconstruction(midindex)
+                    }  
+                }
+            }
+        } else {
+            val n = Math.ceil(Math.log(Ncurrent) / Math.log(2)).toInt + 1
+            for (i <- n to 0 by -1) {
+                for (index <- 0 to Ncurrent by Math.pow(2, i).toInt) {
+                    val startindex = index
+                    val midindex = if (index + Math.pow(2, i-1).toInt > Ncurrent - 1) 0 else index + Math.pow(2, i-1).toInt
+                    val endindex = Math.min(index + Math.pow(2, i).toInt, Ncurrent - 1)
+                    breakable {
+                        if (midindex < 0 || midindex >= Ncurrent || flagreconstruction(midindex)) {
+                            // to the next cycle
+                            break()
+                        }
+                        dataprediction(midindex) = LinearInterpolationPredictor(datareconstruction, startindex, midindex, endindex)
+                        datadelta(midindex) = delta(dataset(midindex), dataprediction(midindex))
+                        dataquantization(midindex) = quantize(datadelta(midindex))
+                        datareconstruction(midindex) = reconstructData(dataprediction(midindex), dataquantization(midindex))
+                        Truereconstruction(midindex)
+                    }   
+                }
+            }
+        }
+        Falsereconstruction() // new quantization step
+        dataquantization
+    }
+}
+
+object SZcompression extends App {
+    import SZData._
+    // compress the quantized values by Huffman encoding
+    case class Node(value: Int, frequency: Int, left: Option[Node] = None, right: Option[Node] = None)
+
+    def buildHuffmanTree(dataquantization: Array[Int]): Node = {
+        if (dataquantization.isEmpty) return Node(-1, 0, None, None)
+        // count the frequency of each value
+        val frequencyMap: mutable.Map[Int, Int] = mutable.Map()
+        dataquantization.foreach(value => {
+            if (frequencyMap.contains(value)) {
+                frequencyMap(value) += 1
+            } else {
+                frequencyMap(value) = 1
+            }
+        })
+
+        // order the values by frequency from low to high
+        val priorityQueue: PriorityQueue[Node] = PriorityQueue()(Ordering.by(node => (-node.frequency, -node.value))) //because max heap, so minus the frequency, low to high     
+        val sortedFrequencyMap = frequencyMap.toSeq.sortBy(_._2)
+        sortedFrequencyMap.foreach { case (value, frequency) =>
+            priorityQueue.enqueue(Node(value, frequency, None, None))
+        }
+
+        // build the huffman tree, left is low, right is high
+        while (priorityQueue.size > 1) {
+            val left = priorityQueue.dequeue()
+            val right = priorityQueue.dequeue()         
+            var parent: Node = Node(left.value + right.value , left.frequency + right.frequency, Some(left), Some(right)) 
+            priorityQueue.enqueue(parent)
+        }
+
+        // return the root of the huffman tree if the priority queue is not empty
+        priorityQueue.dequeue()
+    }
+
+    // print the huffman tree
+    def printTree(root: Node): Unit = {
+        if (root.value == -1 && root.frequency == 0) {
+            println("Empty tree")
+            return
+        }
+        val queue = mutable.Queue[(Node, String)]()
+        queue.enqueue((root, ""))
+        while (queue.nonEmpty) {
+            val levelSize = queue.size
+            val levelNodes = mutable.ArrayBuffer[String]()
+            for (_ <- 0 until levelSize) {
+                val (node, prefix) = queue.dequeue()
+                levelNodes += prefix + s"(${node.value}, ${node.frequency})"
+
+                node.left.foreach(leftNode => queue.enqueue((leftNode, prefix + "  ")))
+                node.right.foreach(rightNode => queue.enqueue((rightNode, prefix + "  ")))
+            }
+            println(levelNodes.mkString(" "))
+        }
+    }
+
+    // generate huffman encoding dictionary for each value
+    def generateHuffmanCodes(huffmanTree: Node): mutable.Map[Int, String] = {
+        if (huffmanTree.value == -1) return mutable.Map()
+
+        val huffmanCodes: mutable.Map[Int, String] = mutable.Map()
+
+        def generateCodes(node: Node, prefix: String): Unit = {
+            if (node.left.isEmpty && node.right.isEmpty) {
+                huffmanCodes(node.value) = prefix
+            } else {
+                node.left.foreach(leftNode => generateCodes(leftNode, prefix + "0"))
+                node.right.foreach(rightNode => generateCodes(rightNode, prefix + "1"))
+            }
+        }
+
+        //return the huffman dictionary
+        if (huffmanTree.left.isEmpty && huffmanTree.right.isEmpty) {
+            huffmanCodes(huffmanTree.value) = "1"
+            huffmanCodes
+        } else {
+            generateCodes(huffmanTree, "")
+        }
+        huffmanCodes
+    }
+
+    // print the huffman encoding dictionary
+    def printHuffmanCodes(encodedictionary: mutable.Map[Int, String]): Unit = {
+        if (encodedictionary.isEmpty) {
+            println("Empty dictionary")
+            return
+        }
+
+        println("Huffman encoding dictionary:")
+        encodedictionary.foreach { case (value, code) =>
+            println(s"$value: $code")
+        }
+    }
+
+    // compress the quantized values by huffman encoding
+    def HuffmanEncoding(quantizedDelta: Array[Int], encodedictionary: mutable.Map[Int, String]): Int = {
+        if (quantizedDelta.isEmpty) return -1
+        // 根据字典压缩数据
+        val compressedData: mutable.StringBuilder = new mutable.StringBuilder()
+        quantizedDelta.foreach(value => {
+            val code = encodedictionary(value)
+            compressedData.append(code)
+        })
+        // turn the binary string to integer
+        Integer.parseInt(compressedData.toString(), 2)
+    }
+}
+
+object SZdecompression extends App {
+
+}
+
+object main extends App {
+    import SZData._
+    import SZprediction._
+    import SZdelta._
+    import SZquantization._
+    import SZreconstruction._
+    import SZpipeline._
+    import SZcompression._
+
 }
--- src/test/scala/example/SZ_test.scala
+++ src/test/scala/example/SZ_test.scala
@@ -1,7 +1,111 @@
 package example
 
-class SZ_test extends munit.FunSuite {
-  test("say hello") {
-    assertEquals(Hello.greeting, "hello")
+import munit.FunSuite
+import org.scalatest.funsuite.AnyFunSuite
+import scala.collection.mutable
+import scala.reflect.runtime.universe
+import scala.reflect.runtime.currentMirror
+
+class SZprediction_test extends FunSuite {
+  test("LinearInterpolationPredictor should predict correctly") {
+    import SZprediction._
+
+    val reconstructData: Array[Double] = Array(0.0, 1.1, 2.2, 3.3, 4.4)
+    val startindex: Int = 0
+    val midindex: Int = 2
+    val endindex: Int = 4
+
+    val predictedData = LinearInterpolationPredictor(reconstructData, startindex, midindex, endindex)
+    val expectedPredicteddata = 2.2
+
+    assert(predictedData == expectedPredicteddata)
+  }
+}
+
+class SZdelta_test extends FunSuite {  
+  test("delta should calculate correctly") {
+    import SZdelta._
+  
+    val predicted: Double = 0.0
+    val actual: Double = 1.1
+    
+    val datadelta: Double = delta(actual, predicted)
+
+    val expectedDeltas: Double = 1.1
+
+    assert(datadelta == expectedDeltas)
+  }
+}
+
+class SZquantization_test extends FunSuite {
+  test("quantize should calculate correctly") {
+    import SZData._
+    import SZquantization._
+
+    val dataprediction: Double = 0.2
+    val expectedQuantizations: Int = 1
+
+    assert(quantize(dataprediction) == expectedQuantizations)
+  }
+}
+
+class SZpipelineTest extends AnyFunSuite {
+  test("quantizationpipeline should return correct quantized data") {
+    import SZData._
+    import SZprediction._
+    import SZdelta._
+    import SZquantization._
+    import SZreconstruction._
+    import SZpipeline._
+
+    dataset = Array(0.8, 1.1, 2.2, 3.3, 4.4, 5.5, 6.6, 7.7)
+    dataprediction = Array.fill(dataset.length)(0.0)
+    datadelta = Array.fill(dataset.length)(0.0)
+    dataquantization = Array.fill(dataset.length)(0)
+    datareconstruction = Array.fill(dataset.length)(0.0)
+    flagreconstruction = Array.fill(dataset.length)(false) // true: reconstructed, false: not reconstructed
+
+    val result: Array[Int] = SZpipeline.quantizationpipeline()
+
+    println(dataset.mkString(" "))
+    println(dataprediction.mkString(" "))
+    println(datadelta.mkString(" "))
+    println(dataquantization.mkString(" "))
+    println(datareconstruction.mkString(" "))
+    
+    val expecedReconstructedData = Array(4, -2, -2, 0, 20, 0, 26, 35)
+    assert(result.sameElements(expecedReconstructedData.map(_.toInt)))
+  }
+}
+
+class SZcompressionTest extends AnyFunSuite {
+  test("HuffmanEncoding should compress dataquantization correctly") {
+    val dataquantization = Array(1, 2, 2, 3, 3, 3, 4, 4, 4, 4)
+    val expectedCompression = Integer.parseInt("1101111111010100000", 2)
+    var encodedictionary = mutable.Map[Int, String]()
+    val huffmanTree = SZcompression.buildHuffmanTree(dataquantization)
+    encodedictionary = SZcompression.generateHuffmanCodes(huffmanTree)
+    val result = SZcompression.HuffmanEncoding(dataquantization, encodedictionary)
+    assert(result == expectedCompression)
+  }
+
+  test("HuffmanEncoding should handle empty array") {
+    val dataquantization = Array[Int]()
+    val expectedCompression = -1
+    var encodedictionary = mutable.Map[Int, String]()
+    val huffmanTree = SZcompression.buildHuffmanTree(dataquantization)
+    encodedictionary = SZcompression.generateHuffmanCodes(huffmanTree)
+    val result = SZcompression.HuffmanEncoding(dataquantization, encodedictionary)
+    assert(result == expectedCompression)
+  }
+
+  test("HuffmanEncoding should handle single element array") {
+    val dataquantization = Array(1)
+    val expectedCompression = Integer.parseInt("1", 2) // 0
+    var encodedictionary = mutable.Map[Int, String]()
+    val huffmanTree = SZcompression.buildHuffmanTree(dataquantization)
+    encodedictionary = SZcompression.generateHuffmanCodes(huffmanTree)
+    val result = SZcompression.HuffmanEncoding(dataquantization, encodedictionary)
+    assert(result == expectedCompression)
   }
 }
